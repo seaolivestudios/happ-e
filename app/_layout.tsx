0a21b276d4fa4bbb1f29ff9e878c5e6b65627dcd
Stephen Olmo <stephen@macbookpro.lan>
2026-05-07 11:06:41 -0400

    Diagnostic: disable native screens to test rn-screens touch interception

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,9 @@ import { ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens(false);
 
 const HappETheme = {
   dark: true,
