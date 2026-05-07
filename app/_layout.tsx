a281756806f57b4c64a6319a9a45e6e9b9b952d8
Stephen Olmo <stephen@macbookpro.lan>
2026-05-07 11:03:50 -0400

    Disable swipe-back gesture on tabs screen to test touch blocking

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout() {
       <Stack initialRouteName="login">
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="light" />
